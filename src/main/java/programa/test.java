6887d2686245127ba4ee6ca8ef85d260bc84d20d
alopezp90 <alopezp90@gmail.com>
2020-11-06 19:29:04 +0100

    Load/Save funcional, iconos inicializados

--- src/main/java/programa/test.java
+++ src/main/java/programa/test.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.io.IOException;
+import javax.swing.*;
 
 /**
  * @author Alberto López Puertas
@@ -18,6 +19,9 @@ public class test {
 //    public static int[] supEstancia = {10, 8, 6, 12, 13};
 //    public static String[] nombreEstancia = {"principal", "", "pequeño", "Paco", "Antonio"};
 //    public static LocalDateTime[] fechaEstancia = new LocalDateTime[tipoEstancia.length];
+    public static final ImageIcon ICONO = new ImageIcon("src/main/resources/icon/icon_96x.jpg");
+    
+    public static int bateria;
     public static int cantidadEstancia;
     public static int[] tipoEstancia;
     public static int[] supEstancia;
@@ -30,6 +34,7 @@ public class test {
 //        }
 //        guarda();
         carga();
+        System.out.println(bateria);
         System.out.println(cantidadEstancia);
         for (int i = 0; i < cantidadEstancia; i++) {
             System.out.println(tipoEstancia[i]);
@@ -47,7 +52,31 @@ public class test {
 //        if (nombreEstancia[1].equals("")) {
 //            System.out.println("cocina esta vacio");
 //        }
-
+        menuSalir();
+    }
+    
+    public static void menuSalir(){
+        int opcion;
+        boolean repite = true;
+        while (repite) {
+            opcion = JOptionPane.showOptionDialog(
+                    null, 
+                    "Indique qué desea hacer:", 
+                    "Hasta la próxima",
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    ICONO,
+                    new Object[] { "Guardar y salir", "Salir sin guardar"},null);
+            switch (opcion){
+                case 0:
+                    repite = false;
+                    guarda();
+                    break;
+                case 1:
+                    repite = false;
+                    break;
+            }
+        }
     }
 
     public static void guarda() {
@@ -82,6 +111,9 @@ public class test {
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             String linea;
+            linea = bufferedReader.readLine();
+            bateria = Integer.parseInt(linea);
+            
             linea = bufferedReader.readLine();
             cantidadEstancia = Integer.parseInt(linea);
 
