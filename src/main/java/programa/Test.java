45a992e14af36765e9703178335028fd09697986
alopezp90 <alopezp90@gmail.com>
2020-11-14 19:32:21 +0100

    Creada la estructura de seleccion de modoLimpieza()

--- src/main/java/programa/Test.java
+++ src/main/java/programa/Test.java
@@ -6,6 +6,8 @@ import java.time.temporal.ChronoUnit;
 import javax.swing.JOptionPane;
 import javax.swing.JLabel;
 import static programa.AspiraDaw.ALERT;
+import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  * @author Alberto López Puertas
@@ -14,30 +16,23 @@ import static programa.AspiraDaw.ALERT;
 public class Test {
 
     public static void main(String[] args) {
-
-        LocalDateTime anterior = LocalDateTime.of(2020, 10, 13, 12, 0);
-        LocalDateTime ahora = LocalDateTime.now();
-
-        LocalDateTime tempDateTime = LocalDateTime.from(anterior);
-
-        long years = tempDateTime.until(ahora, ChronoUnit.YEARS);
-        tempDateTime = tempDateTime.plusYears(years);
-
-        long months = tempDateTime.until(ahora, ChronoUnit.MONTHS);
-        tempDateTime = tempDateTime.plusMonths(months);
-
-        long days = tempDateTime.until(ahora, ChronoUnit.DAYS);
-        tempDateTime = tempDateTime.plusDays(days);
-
-        long hours = tempDateTime.until(ahora, ChronoUnit.HOURS);
-        tempDateTime = tempDateTime.plusHours(hours);
-
-        long minutes = tempDateTime.until(ahora, ChronoUnit.MINUTES);
-
-        System.out.println(years + " years "
-                + months + " months "
-                + days + " days "
-                + hours + " hours "
-                + minutes + " minutes ");
+        
+        int n;
+        
+        String [] usuario;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Numero de usuarios");
+        n = sc.nextInt();
+        usuario = new String[n];
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        for (int i=0; i<usuario.length;i++){
+            System.out.println("Introduce usuario "+(i+1));
+            usuario[i]=scanner.nextLine();
+        }
+        
+        System.out.println(Arrays.toString(usuario));
+        System.out.println(usuario[2]);
     }
 }
